bdaf870b9c05f7d52dd74f297c854ca482aaf3d5
Alexander Freas <akfreas@gmail.com>
2018-02-08 18:43:23 +0100

    Working on.

--- extract.py
+++ extract.py
@@ -2,44 +2,60 @@ from bplist.bplist import BPListReader
 from PIL import Image
 from ipdb import set_trace as bp
 import struct
+import os
+from io import BytesIO
 
 filename = 'AppleName.strings'
 directory = '/System/Library/PrivateFrameworks/CoreEmoji.framework/Versions/A/Resources/en.lproj/'
 
 
-with open('/System/Library/Fonts/Apple Color Emoji.ttc', 'rb') as ttf:
-    
-    pos = 0
-    png_seq = '\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'
-    end_seq = '\x49\x45\x4E\x44'
-    byte = ttf.read(8)
-    current_png = None
-     
-    while byte:
+def extract_pngs_from_ttf():
 
-        if byte == png_seq:
-            current_png = byte
-            print('found png')
-        if byte[:4] == end_seq:
-            current_png += byte[-1]
-            with open('test.png', 'wb') as png_file:
-                png_file.write(current_png)
-            print('wrote file')
-            import os
-            os._exit(0)
+    with open('/System/Library/Fonts/Apple Color Emoji.ttc', 'rb') as ttf:
+        
+        pos = 0
+        png_seq = '\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'
+        end_seq = '\x49\x45\x4E\x44'
+        byte = ttf.read(8)
+        current_png = None
+         
+        while byte:
+
+            if current_png is None:
+                
+                byte = byte[1:]
+                byte += ttf.read(1)
+                if byte == png_seq:
+                    current_png = byte
+            if byte[:4] == end_seq:
+                current_png += byte[-1]
+                with open('test.png', 'wb') as png_file:
+                    png_file.write(current_png)
+                print('wrote file')
+                os._exit(0)
 
 
-        if current_png:
-            lenword = ttf.read(4)
-            length = struct.unpack('>hh', lenword)[1]
-            typ = ttf.read(4)
-            data = ttf.read(length) if length > 0 else ''
-            crc = ttf.read(4)
-            current_png += byte[-1]
-            print('found more bytes')
+            if current_png:
+                lenword = ttf.read(4)
+                length = struct.unpack('>I', lenword)[0]
+                typ = ttf.read(4)
+                data = ttf.read(length) if length > 0 else ''
+                crc = ttf.read(4)
 
-        byte = byte[1:]
-        byte += ttf.read(1)
+                current_png += lenword
+                current_png += typ
+                current_png += data
+                current_png += crc
+
+                if typ == 'IHDR':
+                    width, height = struct.unpack('>II', data[:8])
+                    bp()
+
+                if typ == 'IEND':
+                    bp()
+                    current_png = None
+
+                print('found more bytes', length, typ, ttf.tell())
 
 with open(directory + filename, 'rb') as fp:
     reader = BPListReader(fp.read())
